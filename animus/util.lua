7af12701913e42addf26537fe2b6ba83567d68d9
Leopc1977 <leophancaowz@gmail.com>
2021-05-27 22:08:00 +0200

    Factorisation du code Deplacement vertical

---
+++ animus/util.lua
@@ -0,0 +1,57 @@
+local util = {}
+local width, height = 240,136
+
+function util.Player(pX, pY, pW, pH, pSpeed)
+  local player = {}
+  player.x = pX
+  player.y= pY
+  player.width= pW
+  player.height= pH
+  player.speedY = pSpeed
+  return player
+end
+
+function util.CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
+function util.UpdateBall(ball)
+  ball.x = ball.x + ball.vitesse_x
+  ball.y = ball.y + ball.vitesse_y
+
+  if ball.x < 0 then
+    ball.x = 0
+    ball.vitesse_x = ball.vitesse_x * -1
+  end
+  if ball.y < 0 then
+    ball.y = 0
+    ball.vitesse_y = ball.vitesse_y * -1
+  end
+  if ball.x > width - ball.radius*2 then
+    ball.x =  width - ball.radius2
+    ball.vitesse_x = ball.vitesse_x * -1
+  end
+  if ball.y > height - ball.radius*2 then
+    ball.y = height - ball.radius*2
+    ball.vitesse_y = ball.vitesse_y * -1
+  end
+
+  return ball.x, ball.y, ball.vitesse_x, ball.vitesse_y
+end
+
+function util.UpdateIA(ball, IA, difficulte)
+
+  if (ball.y > IA.y + ball.radius*2 and math.random(0, difficulte) == 1) then
+    IA.speedY = 2
+  elseif (ball.y < IA.y + ball.radius*2 and math.random(0, difficulte) == 1) then
+    IA.speedY = -2
+  end
+  IA.y = IA.y + IA.speedY
+
+  return IA.y
+end
+
+return util
--- scenes/game.lua
+++ scenes/game.lua
@@ -1,27 +1,14 @@
 local game = {}
-
+local util = require("../animus/util")
 local width, height = 240,136
 
-local playerOne = {}
-playerOne.x = 10
-playerOne.width= 50
-playerOne.height= 8
-playerOne.y= 136-playerOne.height
-playerOne.speed = 2
-
-local IA = {}
-IA.x = 10
-IA.width= 50
-IA.height= 8
-IA.y= IA.height
-IA.speed = 2
-IA.speedX = 2
+local playerOne = util.Player(8,10,8,50,2) --(X, Y, W, H, Speed)
+local IA = util.Player(width-8,10,8,50,2)
 
 local ball = {}
-ball.x = 10
+ball.x = 50
 ball.y = 50
-ball.width = 10
-ball.height = 10
+ball.radius = 5
 ball.vitesse_x = 1
 ball.vitesse_y = 1
 
@@ -33,53 +20,27 @@ function game.Update()
   
   --gestion input
   if btn(2) then -- gauche
-    playerOne.x =playerOne.x- playerOne.speed 
+    playerOne.y =playerOne.y- playerOne.speedY
   end
 
   if btn(3) then -- droite
-    playerOne.x =playerOne.x+ playerOne.speed 
+    playerOne.y =playerOne.y+ playerOne.speedY
   end
 
   --gestion IA
-  local difficulte = 5 -- plus la var est grande plus l'IA est fort
-
-  if (ball.x > IA.x + ball.width / 2 and math.random(0, difficulte) == 1) then
-    IA.speedX = 2
-  elseif (ball.x < IA.x + ball.width / 2 and math.random(0, difficulte) == 1) then
-    IA.speedX = -2
-  end
-  IA.x = IA.x + IA.speedX
+  IA.y = util.UpdateIA(ball, IA, 3)
 
   --gestion balle
-  ball.x = ball.x + ball.vitesse_x
-  ball.y = ball.y + ball.vitesse_y
-
-  if ball.x < 0 then
-    ball.vitesse_x = ball.vitesse_x * -1
-  end
-
-  if ball.y < 0 then
-    ball.vitesse_y = ball.vitesse_y * -1
-  end
-  if ball.x > width - ball.width then
-    ball.vitesse_x = ball.vitesse_x * -1
-  end
-  if ball.y > height - ball.height then
-    ball.vitesse_y = ball.vitesse_y * -1
-  end
+  ball.x, ball.y, ball.vitesse_x, ball.vitesse_y = util.UpdateBall(ball)
   
   --gestion de la collision
   --avec le playerOne
-  if ball.y == playerOne.y - ball.height/2 then
-    if ball.x > playerOne.x and ball.x < playerOne.x + playerOne.width then
-      ball.vitesse_y = ball.vitesse_y * -1
-    end
+  if util.CheckCollision(ball.x-ball.radius, ball.y-ball.radius, ball.radius*2, ball.radius*2, playerOne.x, playerOne.y, playerOne.width, playerOne.height)then
+    ball.vitesse_x = -ball.vitesse_x 
   end
   --avec l'IA
-  if ball.y == IA.y + ball.height then
-    if ball.x > IA.x and ball.x < IA.x + playerOne.width then
-      ball.vitesse_y = ball.vitesse_y * -1
-    end
+  if util.CheckCollision(ball.x-ball.radius, ball.y-ball.radius, ball.radius*2, ball.radius*2, IA.x, IA.y, IA.width, IA.height)then
+      ball.vitesse_x = -ball.vitesse_x
   end
 
 end
@@ -88,7 +49,7 @@ function game.Draw()
 
   rect(playerOne.x, playerOne.y,playerOne.width, playerOne.height, 1) -- playerOne
   rect(IA.x, IA.y,IA.width, IA.height, 1) --IA
-  circ(ball.x, ball.y, ball.width/2, 1) -- ball
+  circ(ball.x, ball.y, ball.radius, 1) -- ball
 end
 
 return game
